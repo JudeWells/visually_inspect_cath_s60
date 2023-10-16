eb74b81d71a498064065360f758d2bff4ebdeade
Jude Wells <judewells@hotmail.com>
2023-10-16 17:15:24 +0100

    first commit

---
+++ visually_inspect_images.py
@@ -0,0 +1,50 @@
+import os
+import csv
+from PIL import Image
+from subprocess import run
+
+folder_path = "/Users/judewells/Documents/dataScienceProgramming/data_for_domdet/visualise_cath_s60"
+csv_file = os.path.join(folder_path, "S60_inspection_decisions.csv")
+
+def view_image(image_path):
+    with Image.open(image_path) as img:
+        img.show()
+
+def read_existing_decisions():
+    if not os.path.exists(csv_file):
+        return {}
+    with open(csv_file, mode='r') as file:
+        reader = csv.reader(file)
+        return {rows[0]: rows[1] for rows in reader}
+
+def write_decision(decisions):
+    with open(csv_file, mode='w') as file:
+        writer = csv.writer(file)
+        for key, value in decisions.items():
+            writer.writerow([key, value])
+
+def get_user_decision(file_base):
+    while True:
+        decision = input(f"Viewing {file_base}.png. Enter your decision (1-accept, 2-reject, 0-no decision, 3-view .pse file): ")
+        if decision == "3":
+            run(["pymol", os.path.join(folder_path, f"{file_base}.pse")])
+            continue
+        elif decision in ["1", "2", "0"]:
+            return decision
+        else:
+            print("Invalid option. Please enter 1, 2, 0, or 3.")
+
+def main():
+    decisions = read_existing_decisions()
+
+    for file in sorted(os.listdir(folder_path)):
+        if file.endswith("_combined.png"):
+            file_base = file.replace("_combined.png", "")
+            if file_base not in decisions:
+                view_image(os.path.join(folder_path, file))
+                decision = get_user_decision(file_base)
+                decisions[file_base] = decision
+                write_decision(decisions)
+
+if __name__ == "__main__":
+    main()
