ba5db64e0dc48aadcee79908268357bd19239619
Jude Wells <judewells@hotmail.com>
2023-10-16 17:16:53 +0100

    remove prompts to load pymol

--- visually_inspect_images.py
+++ visually_inspect_images.py
@@ -3,8 +3,8 @@ import csv
 from PIL import Image
 from subprocess import run
 
-folder_path = "/Users/judewells/Documents/dataScienceProgramming/data_for_domdet/visualise_cath_s60"
-csv_file = os.path.join(folder_path, "S60_inspection_decisions.csv")
+folder_path = "../visualise_cath_s60"
+csv_file = os.path.join("S60_inspection_decisions.csv")
 
 def view_image(image_path):
     with Image.open(image_path) as img:
@@ -25,14 +25,14 @@ def write_decision(decisions):
 
 def get_user_decision(file_base):
     while True:
-        decision = input(f"Viewing {file_base}.png. Enter your decision (1-accept, 2-reject, 0-no decision, 3-view .pse file): ")
+        decision = input(f"Viewing {file_base}.png. Enter your decision (1-accept, 2-reject, 0-no decision): ")
         if decision == "3":
             run(["pymol", os.path.join(folder_path, f"{file_base}.pse")])
             continue
         elif decision in ["1", "2", "0"]:
             return decision
         else:
-            print("Invalid option. Please enter 1, 2, 0, or 3.")
+            print("Invalid option. Please enter 1, 2, 0")
 
 def main():
     decisions = read_existing_decisions()
